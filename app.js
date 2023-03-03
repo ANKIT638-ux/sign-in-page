a1c09d8a718f874d32068fc256f1f9da7d0f9e15
Ankit <115006624+ANKIT638-ux@users.noreply.github.com>
2023-03-03 19:22:28 +0530

    OAuth

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require('ejs');
 const mongoose = require("mongoose");
-const bcrypt = require('bcryptjs');
-const salt = bcrypt.genSaltSync(10);
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");   
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 
@@ -15,19 +18,70 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.use(session({
+    secret:"Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.set('strictQuery', true);
 mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+      cb(null, { id: user.id, username: user.username, name: user.name });
+    });
+  });
+   
+  passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, user);
+    });
+  });
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/", function(req, res){
     res.render("home");
 });
+
+app.get("/auth/google",
+    passport.authenticate("google", {scope: ['profile']})
+);
+
+app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect secrets.
+    res.redirect('/secrets');
+  });
 
 app.get("/login", function(req, res){
     res.render("login");
@@ -37,39 +91,64 @@ app.get("/register", function(req, res){
     res.render("register");
 });
 
+app.get("/secrets", function(req, res){
+    // The below line was added so we can't display the "/secrets" page
+    // after we logged out using the "back" button of the browser, which
+    // would normally display the browser cache and thus expose the 
+    // "/secrets" page we want to protect. Code taken from this post.
+    res.set(
+        'Cache-Control', 
+        'no-cache, private, no-store, must-revalidate, max-stal e=0, post-check=0, pre-check=0'
+    );
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    } else{
+        res.redirect("/login");
+    }    
+});
+
+app.get("/logout", function(req, res){
+    req.logOut();
+    res.redirect("/");
+});
+
 app.post("/register", function(req, res){
 
-    const hash = bcrypt.hashSync(req.body.password, salt);
-
-    const newUser = new User({
-        email: req.body.username,
-        password: hash
-    });
-    newUser.save(function(err){
-        if(!err){
-            res.render("secrets");
-        } else {
-            console.log(err);
-        }
-    });
-});
-
-app.post("/login", function(req, res){
-    const username = req.body.username;
-    const password = req.body.password;
-
-    User.findOne({email: username}, function(err, foundUser){
+    User.register({username: req.body.username}, req.body.password, function(err, user){
         if(err){
             console.log(err);
-        } else {
-            if(foundUser){ 
-                if(bcrypt.compareSync(password, foundUser.password)){
-                    res.render("secrets");
-                }
-            }
+            res.redirect("/register");
+        } else{
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            })
+        }
+    })
+});
+
+// this is the new login route, which authenticates first and THEN
+// does the login (which is required to create the session, or so I 
+// understood from the passport.js documentation). 
+// A failed login (wrong password) will give the browser error 
+// "unauthorized".
+ 
+app.post("/login",passport.authenticate("local", {failureRedirect: "/"}), function (req, res) {
+ 
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+ 
+    req.login(user, function(err){
+        if(err){
+            console.log(err);
+            res.redirect("/register");
+        }else{
+            res.redirect("/secrets");
         }
     });
 });
+
 
 app.listen(3000, function(){
     console.log("Running at port 3000");
